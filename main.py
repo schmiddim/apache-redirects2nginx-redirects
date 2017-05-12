31f848febaa0751e0a48bab98f06d28b55b63611
Michael Schmitt <mschmitt@mybestbrands.de>
2017-05-12 15:14:23 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,65 @@
+lines = [line.rstrip('\n') for line in open('htaccess.example')]
+
+routes = []
+
+
+def is_redirect_rule(line):
+    words = line.split()
+    if len(words) < 1:
+        return False
+
+    if words[0] != 'Redirect':
+        return False
+
+    return True
+
+
+def is_410_redirect(line):
+    words = line.split()
+    if words[1] == 'Gone':
+        return True
+    return False
+
+
+def get_status_code(line):
+    if is_410_redirect(line):
+        return "410"
+    words = line.split()
+    return words[1]
+
+
+def get_route(line):
+    words = line.split()
+    return words[2]
+
+
+def get_destination(line):
+    if is_410_redirect(line):
+        return ""
+    words = line.split()
+    return words[3]
+
+
+for line in lines:
+    if is_redirect_rule(line):
+        status_code = get_status_code(line)
+        route = get_route(line)
+        # print(status_code + " "+ route + "  "           +get_destination(line)+ " |" + line)
+        str_redirect = """
+location {} {{
+    rewrite ^(.*)$  {};
+}} """
+        str_gone = """
+location {} {{
+    return 410;
+}}"""
+
+        if route not in routes:
+            if status_code == "301":
+                print('\t' + str_redirect.format(route, get_destination(line), "permanent"))
+            elif status_code == "410":
+                print('\t' + str_gone.format(route, get_destination(line)))
+            else:
+                raise Exception("Unknown status code " + status_code)
+
+        routes.append(route)
